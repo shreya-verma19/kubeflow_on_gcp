6852fe83166065a0384232667010dbeb51a92cdf
Shreya <s.verma1904@gmail.com>
2019-10-11 11:02:34 +0530

    changes to import component

--- pipelines/import_comp/import.py
+++ pipelines/import_comp/import.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('./data/Social_Network_Ads.csv')
+dataset = pd.read_csv('gs://social_network-kubeflow-on-mnist/Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
