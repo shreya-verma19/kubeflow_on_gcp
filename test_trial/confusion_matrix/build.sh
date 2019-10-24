6fd92d2081c749ff4877a21083b8741624be8dbc
Shreya <s.verma1904@gmail.com>
2019-10-24 14:05:26 +0530

    Files added

--- test_trial/confusion_matrix/build.sh
+++ test_trial/confusion_matrix/build.sh
@@ -1,2 +1,3 @@
 IMAGE_PATH=us.gcr.io/kubeflow-on-mnist/cm
 docker build -t $IMAGE_PATH -f Dockerfile .
+docker push $IMAGE_PATH
--- test_trial/feature_eng/build.sh
+++ test_trial/feature_eng/build.sh
@@ -1,5 +1,5 @@
 IMAGE_PATH=us.gcr.io/kubeflow-on-mnist/feature_eng
 docker build -t $IMAGE_PATH -f Dockerfile .
-
+docker push $IMAGE_PATH
 
 
